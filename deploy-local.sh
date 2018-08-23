16cd23962ed67df148e716a339d538ff08fc1968
Martin Ashby <martin@ashbysoft.com>
2018-08-23 21:30:20 +0100

    Updated deployment scripts

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+
+# Build the server project
+pushd sailracetimerserver
+./gradlew build fatJar
+popd
+
+# Build the results widget
+pushd sailraceresults
+REACT_APP_API_SERVER="localhost:8080"
+npm install
+npm run build
+popd
+
+# Copy build products to the static web server folder
+cp -r sailraceresults/build front/static/results
+
 docker-compose down
 docker-compose build
 docker-compose up -d
